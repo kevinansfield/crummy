d47d53970f39be37f72e36cad9a9d9a011dd715e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2012-02-28 17:45:18 +0000

    include html_list separator within li after link/name

--- lib/crummy/standard_renderer.rb
+++ lib/crummy/standard_renderer.rb
@@ -9,18 +9,18 @@ module Crummy
     #
     # Takes 3 options:
     # The output format. Can either be xml or html. Default :html
-    #   :format => (:html|:xml) 
+    #   :format => (:html|:xml)
     # The separator text. It does not assume you want spaces on either side so you must specify. Default +&raquo;+ for :html and +crumb+ for xml
-    #   :separator => string  
+    #   :separator => string
     # Render links in the output. Default +true+
-    #   :link => boolean        
-    # 
+    #   :link => boolean
+    #
     #   Examples:
     #   render_crumbs                         #=> <a href="/">Home</a> &raquo; <a href="/businesses">Businesses</a>
     #   render_crumbs :separator => ' | '     #=> <a href="/">Home</a> | <a href="/businesses">Businesses</a>
     #   render_crumbs :format => :xml         #=> <crumb href="/">Home</crumb><crumb href="/businesses">Businesses</crumb>
     #   render_crumbs :format => :html_list   #=> <ul class="" id=""><li class=""><a href="/">Home</a></li><li class=""><a href="/">Businesses</a></li></ul>
-    #   
+    #
     # With :format => :html_list you can specify additional params: active_li_class, li_class, ul_class, ul_id
     # The only argument is for the separator text. It does not assume you want spaces on either side so you must specify. Defaults to +&raquo;+
     #
@@ -48,8 +48,8 @@ module Crummy
         options[:ul_class] ||= Crummy.configuration.ul_class
         options[:ul_id] ||= Crummy.configuration.ul_id
         crumb_string = crumbs.collect do |crumb|
-          crumb_to_html_list(crumb, options[:links], options[:li_class], options[:active_li_class], options[:first_class], options[:last_class], (crumb == crumbs.first), (crumb == crumbs.last))
-        end.reduce { |memo, obj| memo << obj + options[:separator] }
+          crumb_to_html_list(crumb, options[:links], options[:separator], options[:li_class], options[:active_li_class], options[:first_class], options[:last_class], (crumb == crumbs.first), (crumb == crumbs.last))
+        end.reduce { |memo, obj| memo << obj }
         crumb_string = content_tag(:ul, crumb_string, :class => options[:ul_class], :id => options[:ul_id])
         crumb_string
       when :xml
@@ -70,17 +70,17 @@ module Crummy
       name, url = crumb
       url && links ? link_to(name, url, :class => html_classes) : name
     end
-    
-    def crumb_to_html_list(crumb, links, li_class, active_li_class, first_class, last_class, is_first, is_last)
+
+    def crumb_to_html_list(crumb, links, separator, li_class, active_li_class, first_class, last_class, is_first, is_last)
       name, url = crumb
       html_classes = []
       html_classes << first_class if is_first
       html_classes << last_class if is_last
       html_classes << active_li_class unless url && links
       html_classes << li_class if !is_first && !is_last && url && links
-      content_tag(:li, url && links ? link_to(name, url) : content_tag(:span, name), :class => html_classes.join(' ').strip)
+      content_tag(:li, (url && links ? link_to(name, url) : content_tag(:span, name)) + (separator.html_safe if !is_last), :class => html_classes.join(' ').strip)
     end
-  
+
     def crumb_to_xml(crumb, links, separator, is_first, is_last)
       name, url = crumb
       content_tag(separator, name, :href => (url && links ? url : nil))
